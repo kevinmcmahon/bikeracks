64bf1e11730df0249d692e66d6fd3810f66198a5
Kevin McMahon <kevinmcmahon@users.noreply.github.com>
2012-01-01 20:25:53 -0600

    Initial commit. Data seeding utils

---
+++ data/bike_rack.rb
@@ -0,0 +1,15 @@
+class BikeRack
+    include DataMapper::Resource
+    storage_names[:default] = :bike_racks
+
+    property :id,             Serial, :field => 'id'
+    property :rackid,         Integer
+    property :address,        String
+    property :ward,           Integer
+    property :community_area, Integer
+    property :community_name, String
+    property :totinstall,     Integer
+    property :latitude,       Float
+    property :longitude,      Float
+    property :historical,     Integer
+end
---
+++ data/bike_racks_table.sql
@@ -0,0 +1,24 @@
+﻿
+CREATE TABLE bike_racks (
+	id serial NOT NULL,
+	rackid int,
+	address character varying(50),
+	ward int,
+	community_area int,
+	community_name character varying(50),
+	totinstall int,
+	latitude double precision,
+	longitude double precision,
+	historical int,
+	geom geometry,
+	CONSTRAINT bike_racks_pkey PRIMARY KEY (id),
+	CONSTRAINT enforce_dims_geom CHECK (st_ndims(geom) = 2),
+	CONSTRAINT enforce_geotype_geom CHECK (geometrytype(geom) = 'POINT'::text OR geom IS NULL),
+	CONSTRAINT enforce_srid_geom CHECK (st_srid(geom) = 4326)
+);
+
+CREATE INDEX idx_bike_racks_geom
+ON bike_racks USING gist(geom);
+
+UPDATE bike_racks
+SET geom = ST_GeomFromText('POINT(' || longitude || ' ' || latitude || ')',4326);
