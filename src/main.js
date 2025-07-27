e6f935c078b4371280fa8e56e79b17de6fa812db
Kevin McMahon <kevinmcmahon@users.noreply.github.com>
2025-07-27 13:33:41 -0500

    perf: 📈 self-host Big Shoulders Text font with CHISTAR ligature support
    Replace Google Fonts CDN with locally hosted font files to enable CHISTAR ligature rendering and improve performance through bundling optimization.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from './routes';
 
+// Import Big Shoulders Text font with CHISTAR ligature support
+import './assets/fonts/big-shoulders-text.scss';
+
 const app = createApp(App);
 
 app.use(router);
--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <v-footer padless color="primary" dark>
         <v-col class="text-center footer-text" cols="12">
-            <strong>Kevin McMahon</strong> <span class='chicago-star'>✶</span>  {{ new Date().getFullYear() }} 
+            <strong>Kevin McMahon</strong> <div class='chistar'>CHISTAR</div>  {{ new Date().getFullYear() }} 
         </v-col>
     </v-footer>
 </template>
@@ -15,14 +15,10 @@
   }
   .chistar {
     display: inline;
-    font-family: "Big Shoulders Text";
+    font-family: "Big Shoulders Text", sans-serif;
     color: vars.$color-star-red;
     font-variant-ligatures: discretionary-ligatures;
     font-feature-settings: "dlig" 1;
-  }
-  .chicago-star {
-    display: inline;
-    color: vars.$color-star-red;
-    font-size: 1.2em;
+    font-weight: 500;
   }
 </style>
